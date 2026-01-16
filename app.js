e98ac1769a39a00a9f90dbf6f16cb88adc379612
emanuelelisciandrello511-star <emanuele.lisciandrello511@gmail.com>
2026-01-16 01:55:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
+alert("✅ app.js caricato su Netlify!");
+document.body.insertAdjacentHTML("afterbegin", "<div style='padding:12px;font-weight:700'>✅ JS OK</div>");
+console.log("✅ app.js caricato su Netlify!");
 
